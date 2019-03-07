ea41ca5857210d362a91d7e3a39ec39eb516b629
Kurt King <kurtking@Kurts-MacBook-Pro.local>
2019-03-06 18:11:51 -0600

    save

--- clicky-app/src/App.js
+++ clicky-app/src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Card from "./components/Card";
+import chars from "./cards.json";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
 import './App.css';
 
+
 class App extends Component {
+  // Setting this.state.pups to the cards json array
+  state = {
+    chars,
+    clickedChars: [],
+    score: 0,
+    topScore:0,
+    guessRight:undefined,
+    winner:false
+    
+  };
+
+  
+
+  shuffleScoreCard = id => {
+  
+    chars.sort(() => Math.random() - 0.5);
+
+    this.setState({ chars, score:   this.state.score +1 });
+
+
+
+    
+
+
+
+      
+
+    
+
+  };
+
+  
+
+  
+  
+
+  
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Car Clicker</h1>
+        <p className="App-intro">
+          Try not to click the same image twice!
+        </p>
+      </header>
+      <Score total={this.state.score}
+             topScore={this.state.topScore}
+             status={this.state.status}
+             />
+      <Wrapper>
+        {this.state.chars.map(data => (
+          <Card
+            shuffleScoreCard={this.shuffleScoreCard}
+            id={data.id}
+            key={data.id}
+            image={data.image}
+          />
+        ))}
+      </Wrapper>
+    
+  </div>
+  );
+}
 }
 
 export default App;
---
+++ clicky-app/src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Card.css";
+
+//this component is only rendering the images to the screen and can be a simple render function it is a
+//stateless component
+const Card = props => (
+  <div className="card img-container hover">
+      <img alt={props.name} src={props.image} id={props.id}/>
+        {/* // onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore'/> */}
+        <span onClick={() => props.shuffleScoreCard(props.id)} className="remove">
+        𝘅
+      </span>
+  </div>
+);
+
+export default Card;

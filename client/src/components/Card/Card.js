8b137a9a5ec262d704f104f1ac0d032f8e1ac1d2
Kurt King <kurtking@Kurts-MacBook-Pro.local>
2019-03-06 18:59:01 -0600

    cards

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -5,7 +5,6 @@ const Card = props => (
   <div className="card img-container hover">
     <img alt={props.name} src={props.image} id={props.id} />
     <span onClick={() => props.shuffleScoreCard(props.id)} className="remove">X</span>
-
   </div>
 );
 

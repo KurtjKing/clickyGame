3a81c0966c5dee79d70a793c5bf210155b63e665
Kurt King <kurtking@Kurts-MacBook-Pro.local>
2019-03-08 13:05:52 -0600

    files

---
+++ build/precache-manifest.51fdacc8fbeffd4498abeae71a611e77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe81c4c31232682d87b",
+    "url": "/clickyGame/static/js/runtime~main.3fe81c4c.js"
+  },
+  {
+    "revision": "02181104d36663ed7416",
+    "url": "/clickyGame/static/js/main.02181104.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/clickyGame/static/js/2.ff661de6.chunk.js"
+  },
+  {
+    "revision": "02181104d36663ed7416",
+    "url": "/clickyGame/static/css/main.5594e8f5.chunk.css"
+  },
+  {
+    "revision": "5017f407e47713d14e7d9fdad45363e6",
+    "url": "/clickyGame/index.html"
+  }
+];

050a42294a08e77bd5ffcc318c3c0bb6ee196983
Kurt King <kurtking@dhcp-10-105-216-201.wireless.northwestern.private>
2019-03-09 09:52:20 -0600

    Updates

---
+++ precache-manifest.24c292b256e02645bef9b758ef4a253f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe81c4c31232682d87b",
+    "url": "/clickyGame/static/js/runtime~main.3fe81c4c.js"
+  },
+  {
+    "revision": "5f36ad39020e3211da2b",
+    "url": "/clickyGame/static/js/main.5f36ad39.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/clickyGame/static/js/2.ff661de6.chunk.js"
+  },
+  {
+    "revision": "5f36ad39020e3211da2b",
+    "url": "/clickyGame/static/css/main.05131559.chunk.css"
+  },
+  {
+    "revision": "f2b36206201a058fda113d80f188519b",
+    "url": "/clickyGame/index.html"
+  }
+];
